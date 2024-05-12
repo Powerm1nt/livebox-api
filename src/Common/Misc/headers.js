253afce7e98553cbfd1a967a14a227573eeea0df
Leonard Chabot <leonard.chabot@nuka.works>
2024-05-12 18:06:32 +0200

    Removed debug code

--- src/Common/Misc/headers.js
+++ src/Common/Misc/headers.js
@@ -1,10 +1,8 @@
 export const headers = (token = "", sessionId = "") => {
-	const headers = {
+	return {
 		"Content-Type": "application/x-sah-ws-4-call+json",
 		Authorization: token?.length > 0 ? `X-Sah ${token}` : 'X-Sah-Login',
         "X-Context": token,
         Cookie: `475ec806/sessid=${sessionId}; sah/contextId=${token};`
 	};
-    console.log('Headers:', headers)
-    return headers
 };
