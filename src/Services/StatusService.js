37906f2f602e28eeaf3a5437c5e79d2b2589f8ef
Leonard Chabot <leonard.chabot@nuka.works>
2024-05-12 18:36:24 +0200

    StatusService: Added reboot and check-updates

--- src/Services/StatusService.js
+++ src/Services/StatusService.js
@@ -1,11 +1,46 @@
 import express from "express";
-import { error, query, success } from "../Common/Misc/utils.js";
-import { headers } from "../Common/Misc/headers.js";
-
+import { login, query } from "../Common/Misc/utils.js";
+import env from "../Common/Misc/ConfigProvider.mjs";
 export const statusService = express.Router();
 
-statusService.get("/", async (req, res, next) => {
-    const apiResponse = await query("DeviceInfo", "get", { parameters: {} });
+statusService.get("/info", async (req, res, next) => {
+    const user = await login(env.USERNAME, env.PASSWORD);
+	if (!user.success) return res.status(500).json(user);
+    const apiResponse = await query("DeviceInfo", "get", { parameters: {} }, user.data.data.contextID, user.data.sessionId);
+
+	// Si la requete n'a pas été successful
+	if (!apiResponse.success) return res.status(500).json(apiResponse);
+
+	return res.json(apiResponse);
+});
+
+statusService.get("/reboot", async (req, res, next) => {
+    const user = await login(env.USERNAME, env.PASSWORD);
+	if (!user.success) return res.status(500).json(user);
+    const apiResponse = await query(
+        "NMC",
+        "reboot",
+        { parameters: { reason: 'GUI_Reboot' } },
+        user.data.data.contextID,
+        user.data.sessionId
+    );
+
+	// Si la requete n'a pas été successful
+	if (!apiResponse.success) return res.status(500).json(apiResponse);
+
+	return res.json(apiResponse);
+});
+
+statusService.get("/check-updates", async (req, res, next) => {
+    const user = await login(env.USERNAME, env.PASSWORD);
+	if (!user.success) return res.status(500).json(user);
+    const apiResponse = await query(
+        "NMC",
+        "checkForUpgrades",
+        { parameters: {} },
+        user.data.data.contextID,
+        user.data.sessionId
+    );
 
 	// Si la requete n'a pas été successful
 	if (!apiResponse.success) return res.status(500).json(apiResponse);
